c944b1b1c35f4b18649b1907e0bcc3428e00ef1b
Vitaly <vitaly.rybkin@gmail.com>
2023-02-14 16:36:23 +0300

    14. Longest Common Prefix

---
+++ 14_Longest_Common_Prefix.py
@@ -0,0 +1,25 @@
+"""
+Write a function to find the longest common prefix string amongst an array of strings.
+
+If there is no common prefix, return an empty string "".
+"""
+
+strs = ["flower","flow","flight"]
+
+
+def longestCommonPrefix(strs: list[str]) -> str:
+    res = ""
+    first = strs[0]
+    prefs_dict = {}
+    for i in range(len(min(strs))):
+        prefs_dict[first[0 : i + 1]] = i
+        for j in range(len(strs)):
+            if strs[j][0 : i + 1] in prefs_dict.keys():
+                if j == len(strs) - 1:
+                    res = first[0 : i + 1]
+            else:
+                return res
+    return res
+
+
+print(longestCommonPrefix(strs))
