50b38bd225dacc307f98142a5976655bb3bf8202
Vitaly <vitaly.rybkin@gmail.com>
2023-03-09 11:44:27 +0300

    202. Happy Number

---
+++ 202_Happy_Number.py
@@ -0,0 +1,27 @@
+"""
+Write an algorithm to determine if a number n is happy.
+
+A happy number is a number defined by the following process:
+
+Starting with any positive integer, replace the number by the sum of the squares of its digits.
+Repeat the process until the number equals 1 (where it will stay),
+or it loops endlessly in a cycle which does not include 1.
+Those numbers for which this process ends in 1 are happy.
+Return true if n is a happy number, and false if not.
+"""
+
+n = 2
+
+def isHappy(n: int) -> bool:
+    summum = 0
+    while not summum == 1:
+        summum = 0
+        while not n == 0:
+            summum += pow(n % 10, 2)
+            n //= 10
+        n = summum
+        if summum == 145:
+            return False
+    return  True
+
+print(isHappy(n))
